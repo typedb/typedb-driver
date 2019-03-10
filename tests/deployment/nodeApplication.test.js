31181d0ecb39cbdee4a94a527c091ffcd6be05d0
Haikal Pribadi <haikal@grakn.ai>
2019-03-10 23:17:23 +0000

    Fixed async function syntax

--- tests/deployment/nodeApplication.test.js
+++ tests/deployment/nodeApplication.test.js
@@ -1,13 +1,13 @@
 const GraknClient = require("grakn-client");
 
 
-beforeEach(async () => {
+beforeEach(async function() => {
     client = new GraknClient("localhost:48555");
     session = await client.session("testkeyspace");
     tx = await session.transaction().write();
 })
 
-afterEach(async () => {
+afterEach(async function() => {
     await tx.close();
     await session.close();
     client.close();
@@ -18,16 +18,16 @@ afterEach(async () => {
 
 describe("Basic GraknClient Tests", () => {
 
-    test("define", async () => {
+    test("define", async function() => {
         const defined = await tx.query("define person sub entity, has name; name sub attribute, datatype string;");
         tx.commit();
     });
 
-    test("match", async () => {
+    test("match", async function() => {
         const types = await tx.query("match $x sub thing; get;");
     });
 
-    test("insert", async () => {
+    test("insert", async function() => {
         const defined = await tx.query("define person sub entity, has name; name sub attribute, datatype string;");
         const inserted = await tx.query("insert $x isa person, has name \"john\";");
     });
