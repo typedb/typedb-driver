ac8766cd034e282d790cc9df9f3297ea1d39fac5
Haikal Pribadi <haikal@grakn.ai>
2019-03-11 00:02:18 +0000

    Fixed deployment test, and optimised CI workflow

--- tests/deployment/application.test.js
+++ tests/deployment/application.test.js
@@ -1,13 +1,13 @@
 const GraknClient = require("grakn-client");
 
 
-beforeEach(async function() => {
+beforeEach(async function() {
     client = new GraknClient("localhost:48555");
     session = await client.session("testkeyspace");
     tx = await session.transaction().write();
 })
 
-afterEach(async function() => {
+afterEach(async function() {
     await tx.close();
     await session.close();
     client.close();
@@ -18,16 +18,16 @@ afterEach(async function() => {
 
 describe("Basic GraknClient Tests", () => {
 
-    test("define", async function() => {
+    test("define", async function() {
         const defined = await tx.query("define person sub entity, has name; name sub attribute, datatype string;");
         tx.commit();
     });
 
-    test("match", async function() => {
+    test("match", async function() {
         const types = await tx.query("match $x sub thing; get;");
     });
 
-    test("insert", async function() => {
+    test("insert", async function() {
         const defined = await tx.query("define person sub entity, has name; name sub attribute, datatype string;");
         const inserted = await tx.query("insert $x isa person, has name \"john\";");
     });
