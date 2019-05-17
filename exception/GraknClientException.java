ea5d699aeaef172b469cf02bac3250020ab36385
Marco Scoppetta <marco@grakn.ai>
2019-05-17 12:35:04 +0100

    make status code available in GraknClientException

--- exception/GraknClientException.java
+++ exception/GraknClientException.java
@@ -21,22 +21,28 @@ package grakn.client.exception;
 
 import grakn.core.common.exception.ErrorMessage;
 import grakn.core.common.exception.GraknException;
+import io.grpc.StatusRuntimeException;
+
+import javax.annotation.Nullable;
 
 public class GraknClientException extends GraknException {
+
+    private String statusCode;
 
     GraknClientException(String error) {
         super(error);
     }
 
-    protected GraknClientException(String error, Exception e) {
+    protected GraknClientException(String error, StatusRuntimeException e) {
         super(error, e);
+        this.statusCode = e.getStatus().getCode().name();
     }
 
     public static GraknClientException create(String error) {
         return new GraknClientException(error);
     }
 
-    public static GraknClientException create(String error, Exception e) {
+    public static GraknClientException create(String error, StatusRuntimeException e) {
         return new GraknClientException(error, e);
     }
 
@@ -52,4 +58,7 @@ public class GraknClientException extends GraknException {
     public String getName() {
         return this.getClass().getName();
     }
+
+    @Nullable
+    public String getStatusCode() { return this.statusCode; }
 }
