86377420b62d7bcbae608cbe996f59e0a0f279a4
alexjpwalker <alex@grakn.ai>
2020-06-18 14:07:13 +0100

    Remove more wildcard imports from GraqlSteps.java

--- test/behaviour/graql/GraqlSteps.java
+++ test/behaviour/graql/GraqlSteps.java
@@ -59,7 +59,12 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 public class GraqlSteps {
 
