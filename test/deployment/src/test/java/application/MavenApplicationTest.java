015ddd0cbfdc4bc25121085b824393eef830870e
alexjpwalker <alex@grakn.ai>
2020-08-13 20:00:10 +0100

    Add bazelignore and fix maven test

--- test/deployment/src/test/java/application/MavenApplicationTest.java
+++ test/deployment/src/test/java/application/MavenApplicationTest.java
@@ -1,5 +1,9 @@
 package application;
 
+import grakn.client.Grakn;
+import grakn.client.Grakn.Client;
+import grakn.client.Grakn.Session;
+import grakn.client.Grakn.Transaction;
 import grakn.client.answer.ConceptMap;
 import graql.lang.Graql;
 import org.junit.Test;
@@ -12,8 +16,8 @@ import static org.junit.Assert.assertEquals;
 public class MavenApplicationTest {
     @Test
     public void testImport() {
-        GraknClientOld client = new GraknClientOld("localhost:48555");
-        GraknClientOld.Session session = client.session("grakn");
+        Client client = Grakn.client("localhost:48555");
+        Session session = client.session("grakn");
         Transaction tx = session.transaction().write();
         List<ConceptMap> answers = tx.execute(Graql.match(var("t").sub("thing")).get()).get();
         tx.close();
