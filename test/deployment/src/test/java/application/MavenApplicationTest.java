06c4207e975c0392bc9c4dc1ea87193305c1a78c
haikalpribadi <haikal@grakn.ai>
2020-09-12 17:21:46 +0100

    Fixed test-deployment-maven
    1) The test still used the old API Grakn.client(..) which has been removed
    2) The test used Java's try-with-resource feature which is only available in Java 7+

--- test/deployment/src/test/java/application/MavenApplicationTest.java
+++ test/deployment/src/test/java/application/MavenApplicationTest.java
@@ -20,10 +20,10 @@
 package application;
 
 import grakn.client.Grakn;
-import grakn.client.Grakn.Client;
 import grakn.client.Grakn.Session;
 import grakn.client.Grakn.Transaction;
 import grakn.client.concept.type.ThingType;
+import grakn.client.rpc.GraknClient;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -34,15 +34,16 @@ public class MavenApplicationTest {
 
     @Test
     public void test() {
-        try (Client client = Grakn.client("localhost:48555")) {
-            client.databases().create("grakn");
-            try (Session session = client.session("grakn")) {
-                try (Transaction tx = session.transaction(Transaction.Type.WRITE)) {
-                    ThingType root = tx.concepts().getRootThingType();
-                    assertNotNull(root);
-                    assertEquals(4, root.asRemote(tx).getSubtypes().count());
-                }
-            }
-        }
+        Grakn.Client client = new GraknClient("localhost:48555");
+        client.databases().create("grakn");
+        Session session = client.session("grakn");
+        Transaction tx = session.transaction(Transaction.Type.WRITE);
+        ThingType root = tx.concepts().getRootThingType();
+        assertNotNull(root);
+        assertEquals(4, root.asRemote(tx).getSubtypes().count());
+
+        tx.close();
+        session.close();
+        client.close();
     }
 }
