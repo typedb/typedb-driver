170a826720916fd6c849ca4ae0a31b96a87f3076
Alex Walker <alex@grakn.ai>
2020-10-21 19:34:47 +0100

    Rename ResponseCollector to RPCResponseAccumulator

--- rpc/RPCResponseAccumulator.java
+++ rpc/RPCResponseAccumulator.java
@@ -17,7 +17,7 @@
  * under the License.
  */
 
-package grakn.client.rpc.response;
+package grakn.client.rpc;
 
 import grakn.protocol.TransactionProto;
 
@@ -26,32 +26,29 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-public class ResponseCollector {
-    private volatile boolean isDone;
-    private final BlockingQueue<Response> received = new LinkedBlockingQueue<>();
+public class RPCResponseAccumulator {
+    private final AtomicBoolean isDone;
+    private final BlockingQueue<RPCResponse> received = new LinkedBlockingQueue<>();
 
-    public ResponseCollector() {
-        isDone = false;
+    public RPCResponseAccumulator() {
+        isDone = new AtomicBoolean(false);
     }
 
-    public void onResponse(final Response response) {
-        received.add(response);
-        TransactionProto.Transaction.Res res = response.response();
-        if (res == null || isLastResponse(res)) {
-            isDone = true;
-        }
+    public void onResponse(final RPCResponse rpcResponse) {
+        received.add(rpcResponse);
+        if (!(rpcResponse instanceof RPCResponse.Ok) || isLastResponse(rpcResponse.get())) isDone.compareAndSet(false, true);
     }
 
     public boolean isDone() {
-        return isDone;
+        return isDone.get();
     }
 
     public TransactionProto.Transaction.Res take() throws InterruptedException {
-        return received.take().ok();
+        return received.take().get();
     }
 
     public TransactionProto.Transaction.Res take(long timeout, TimeUnit unit) throws InterruptedException {
-        return received.poll(timeout, unit).ok();
+        return received.poll(timeout, unit).get();
     }
 
     boolean isLastResponse(final TransactionProto.Transaction.Res response) {
