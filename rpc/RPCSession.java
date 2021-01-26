8f7cf33539175c537a906d305bdf68b4ff3de612
Ganeshwara Hananda <ganeshwara@grakn.ai>
2021-01-26 12:49:10 +0000

    Remove redundant checks which is already done by asCluster()

--- rpc/RPCSession.java
+++ rpc/RPCSession.java
@@ -181,7 +181,6 @@ public class RPCSession {
 
         @Override
         public Grakn.Transaction transaction(Grakn.Transaction.Type type, GraknOptions options) {
-            if (!options.isCluster()) throw new GraknClientException(ILLEGAL_CAST, options);
             GraknOptions.Cluster clusterOpt = options.asCluster();
             if (clusterOpt.allowSecondaryReplica().isPresent() && clusterOpt.allowSecondaryReplica().get()) {
                 return transactionSecondaryReplica(type, clusterOpt);
