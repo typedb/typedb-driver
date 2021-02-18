76ba4eec4c4e0b70b0af6cd69b38aa8d71ee1616
Alex Walker <alex@grakn.ai>
2021-02-18 14:17:32 +0000

    Don't print inner exception of connection failure
    Because it breaks the parse logic we need in Cluster.

--- common/exception/GraknClientException.java
+++ common/exception/GraknClientException.java
@@ -42,7 +42,7 @@ public class GraknClientException extends RuntimeException {
     public static GraknClientException of(StatusRuntimeException statusRuntimeException) {
         // "Received Rst Stream" occurs if the server is in the process of shutting down.
         if (statusRuntimeException.getStatus().getCode() == Status.Code.UNAVAILABLE || statusRuntimeException.getMessage().contains("Received Rst Stream")) {
-            return new GraknClientException(ErrorMessage.Client.UNABLE_TO_CONNECT.message(), statusRuntimeException);
+            return new GraknClientException(ErrorMessage.Client.UNABLE_TO_CONNECT);
         } else if (isReplicaNotPrimaryException(statusRuntimeException)) {
             return new GraknClientException(ErrorMessage.Client.CLUSTER_REPLICA_NOT_PRIMARY);
         }
