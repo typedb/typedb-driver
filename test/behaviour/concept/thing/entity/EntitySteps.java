79441bfd78ab95ccc2a9203d3f5354ba3bf42ac4
haikalpribadi <haikal@grakn.ai>
2021-03-17 15:15:57 +0000

    Fixed BDD steps

--- test/behaviour/concept/thing/entity/EntitySteps.java
+++ test/behaviour/concept/thing/entity/EntitySteps.java
@@ -21,6 +21,7 @@ package grakn.client.test.behaviour.concept.thing.entity;
 
 import grakn.client.api.concept.thing.Attribute;
 import grakn.client.api.concept.thing.Entity;
+import grakn.client.common.Label;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
@@ -73,19 +74,19 @@ public class EntitySteps {
     @When("{var} = entity\\( ?{type_label} ?) get instance with key\\( ?{type_label} ?): {long}")
     public void entity_type_get_instance_with_key(String var1, String type, String keyType, long keyValue) {
         put(var1, tx().concepts().getAttributeType(keyType).asLong().asRemote(tx()).get(keyValue).asRemote(tx()).getOwners()
-                .filter(owner -> owner.getType().getLabel().equals(type)).findFirst().orElse(null));
+                .filter(owner -> owner.getType().getLabel().equals(Label.of(type))).findFirst().orElse(null));
     }
 
     @When("{var} = entity\\( ?{type_label} ?) get instance with key\\( ?{type_label} ?): {word}")
     public void entity_type_get_instance_with_key(String var1, String type, String keyType, String keyValue) {
         put(var1, tx().concepts().getAttributeType(keyType).asString().asRemote(tx()).get(keyValue).asRemote(tx()).getOwners()
-                .filter(owner -> owner.getType().getLabel().equals(type)).findFirst().orElse(null));
+                .filter(owner -> owner.getType().getLabel().equals(Label.of(type))).findFirst().orElse(null));
     }
 
     @When("{var} = entity\\( ?{type_label} ?) get instance with key\\( ?{type_label} ?): {datetime}")
     public void entity_type_get_instance_with_key(String var1, String type, String keyType, LocalDateTime keyValue) {
         put(var1, tx().concepts().getAttributeType(keyType).asDateTime().asRemote(tx()).get(keyValue).asRemote(tx()).getOwners()
-                .filter(owner -> owner.getType().getLabel().equals(type)).findFirst().orElse(null));
+                .filter(owner -> owner.getType().getLabel().equals(Label.of(type))).findFirst().orElse(null));
     }
 
     @Then("entity\\( ?{type_label} ?) get instances contain: {var}")
