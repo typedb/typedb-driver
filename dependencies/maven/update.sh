f7002a6a866dce8b9e064865deaa5feecc82e112
haikalpribadi <haikal@vaticle.com>
2021-05-10 16:47:42 +0100

    Added copyright to Apache license headers

--- dependencies/maven/update.sh
+++ dependencies/maven/update.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 #
+# Copyright (C) 2021 Vaticle
+#
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
