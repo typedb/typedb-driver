9f0469d2f6cd42a7cf5a10821518e445288c023b
Ganeshwara Hananda <ganesh@vaticle.com>
2021-05-26 20:05:08 +0100

    Remove TODOs

--- api/user/UserManager.java
+++ api/user/UserManager.java
@@ -9,7 +9,6 @@ public interface UserManager {
 
     @CheckReturnValue
     boolean contains(String name);
-    // TODO: Return type should be 'Database' but right now that would require 2 server calls in Cluster
 
     void create(String name, String password);
 
