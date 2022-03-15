bb9715a36ece003f51ef695c7aad2cb8e4c6a902
Alex Walker <alex@vaticle.com>
2022-03-15 10:03:01 +0000

    Error message system

--- common/error/message.rs
+++ common/error/message.rs
@@ -18,37 +18,15 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-//
-// macro_rules! error {
-//     ($fn_name:ident, $number:literal, $text:literal) => {
-//         pub(crate) fn $fn_name() -> String {
-//             format!("[CLI{:0>2}]: {}", $number, $text)
-//         }
-//     };
-// }
-// macro_rules! error_args_1 {
-//     ($fn_name:ident, $number:literal, $text:literal) => {
-//         pub(crate) fn $fn_name(arg0: &str) -> String {
-//             format!("[CLI{:0>2}]: {}", $number, format!($text, arg0))
-//         }
-//     };
-// }
-// macro_rules! error_args_2 {
-//     ($fn_name:ident, $number:literal, $text:literal) => {
-//         pub(crate) fn $fn_name(arg0: &str, arg1: &str) -> String {
-//             format!("[CLI{:0>2}]: {}", $number, format!($text, arg0, arg1))
-//         }
-//     };
-// }
 
-pub(super) struct MessageTemplate<'a> {
+pub(super) struct Template<'a> {
     code_prefix: &'a str,
     msg_prefix: &'a str,
 }
 
-impl MessageTemplate<'_> {
-    const fn new<'a>(code_prefix: &'a str, msg_prefix: &'a str) -> MessageTemplate<'a> {
-        MessageTemplate {
+impl Template<'_> {
+    pub(super) const fn new<'a>(code_prefix: &'a str, msg_prefix: &'a str) -> Template<'a> {
+        Template {
             code_prefix,
             msg_prefix
         }
@@ -63,7 +41,7 @@ pub struct Message<'a> {
 }
 
 impl Message<'_> {
-    const fn new<'a>(template: MessageTemplate<'a>, code_number: u8, msg_body: &'a str) -> Message<'a> {
+    pub(super) const fn new<'a>(template: Template<'a>, code_number: u8, msg_body: &'a str) -> Message<'a> {
         Message {
             code_prefix: template.code_prefix,
             code_number,
@@ -72,79 +50,27 @@ impl Message<'_> {
         }
     }
 
-    pub(crate) fn message(&self, args: Vec<&str>) -> String {
-        if self.msg_body.contains("{}") {
-            format!("[{}{:0>2}] {}: {}", self.code_prefix, self.code_number, self.msg_prefix,
-                    self.msg_body.clone().replace("{}", args[0]))
-        } else {
-            format!("[{}{:0>2}] {}: {}", self.code_prefix, self.code_number, self.msg_prefix, self.msg_body.clone())
+    pub(crate) fn format(&self, args: Vec<&str>) -> String {
+        let expected_arg_count = self.msg_body.matches("{}").count();
+        assert_eq!(expected_arg_count, args.len());
+        format!("[{}{:0>2}] {}: {}", self.code_prefix, self.code_number, self.msg_prefix, self.expand_msg(args))
+    }
+
+    fn expand_msg(&self, args: Vec<&str>) -> String {
+        let arg_count = args.len();
+        let msg_split_indexed = self.msg_body.split("{}").enumerate();
+        let mut formatted_msg = String::new();
+        for (idx, fragment) in msg_split_indexed {
+            formatted_msg.push_str(fragment);
+            if idx < arg_count { formatted_msg.push_str(args[idx]) }
         }
+        formatted_msg
     }
 }
 
-// impl Message::Args0 {
-//     fn get() -> String {
-//         format!("[{}{:0>2}] {}: {}", msg.code_prefix, msg.code_number, msg.msg_prefix, msg.msg_body)
-//     }
-// }
-//
-// impl Message::Args1 {
-//     fn format(arg0: &str) -> String {
-//         format!("[{}{:0>2}] {}: ", msg.code_prefix, msg.code_number, msg.msg_prefix) + msg.msg_body
-//     }
-// }
-
-// impl From<Message> for String {
-//     fn from(msg: Message) -> Self {
-//         format!("[{}{:0>2}] {}: {}", msg.code_prefix, msg.code_number, msg.msg_prefix, msg.msg_body)
-//     }
-// }
-
-struct Templates<'a> {
-    client: MessageTemplate<'a>
-}
-
-impl Templates<'_> {
-    const fn new() -> Templates<'static> {
-        Templates {
-            client: MessageTemplate::new("CLI", "Client Error")
-        }
+impl From<Message<'_>> for String {
+    fn from(msg: Message) -> Self {
+        assert!(!msg.msg_body.contains("{}"));
+        String::from(msg.msg_body)
     }
 }
-
-const TEMPLATES: Templates = Templates::new();
-
-pub struct ErrorMessages<'a> {
-    pub transaction_closed_with_errors: Message<'a>,
-    pub unable_to_connect: Message<'a>
-}
-
-impl ErrorMessages<'_> {
-    const fn new() -> ErrorMessages<'static> {
-        ErrorMessages {
-            transaction_closed_with_errors: Message::new(TEMPLATES.client, 4, "The transaction has been closed with error(s): \n{}"),
-            unable_to_connect: Message::new(TEMPLATES.client, 5, "Unable to connect to TypeDB server.")
-        }
-    }
-}
-
-pub const ERRORS: ErrorMessages = ErrorMessages::new();
-
-// pub(crate) mod client {
-//     use super::Message;
-//
-//     const PREFIX: Message = Message { code_prefix: "CLI", msg_prefix: "Client Error", ..Default::default() };
-//
-//     pub const UNABLE_TO_CONNECT: Message = Message { code_number: 5, msg_body: "Unable to connect to TypeDB server.", ..PREFIX };
-//
-//     // client_error_args_1!(transaction_closed_with_errors, 4, "The transaction has been closed with error(s): \n{}");
-//     // client_error!(unable_to_connect, 5, "Unable to connect to TypeDB server.");
-//
-//     pub fn session_id_exists(id: &str) -> String {
-//         format!("The newly opened session id '{}' already exists.", id)
-//     }
-//
-//     pub fn session_closed() -> &'static str {
-//         "The session has been closed and no further operation is allowed."
-//     }
-// }
--- common/error/mod.rs
+++ common/error/mod.rs
@@ -19,13 +19,15 @@
  * under the License.
  */
 
-use grpc::Error as GrpcError;
+pub mod message;
+pub mod messages;
+
+use grpc::{Error as GrpcError, GrpcMessageError, GrpcStatus};
 use std::error::Error as StdError;
 use std::fmt::{Debug, Display, Formatter};
 
-pub mod message;
-
-use super::error::message::Message;
+use crate::common::error::message::Message;
+use crate::common::error::messages::ERRORS;
 
 #[derive(Debug)]
 pub enum Error {
@@ -52,18 +54,33 @@ impl StdError for Error {
     }
 }
 
-// impl From<Message<'_>> for Error {
-//     fn from(msg: Message) -> Self {
-//         Error::new(msg)
-//     }
-// }
-
 impl Error {
-    pub fn new(msg: &str) -> Error {
+    pub fn new(msg: Message) -> Error {
         Error::Other(String::from(msg))
     }
 
-    pub fn from_grpc(msg: String, source: GrpcError) -> Error {
-        Error::GrpcError(msg, source)
+    pub fn format(msg: Message, args: Vec<&str>) -> Error {
+        Error::Other(msg.format(args))
+    }
+
+    pub fn from_grpc(source: GrpcError) -> Error {
+        match source {
+            GrpcError::Http(_) => Error::GrpcError(String::from(ERRORS.client.unable_to_connect), source),
+            GrpcError::GrpcMessage(ref err) => {
+                if Error::is_replica_not_primary(err) { Error::new(ERRORS.client.cluster_replica_not_primary) }
+                else if Error::is_token_credential_invalid(err) { Error::new(ERRORS.client.cluster_token_credential_invalid) }
+                else { Error::GrpcError(source.to_string().replacen("grpc message error: ", "", 1), source) }
+            },
+            _ => Error::GrpcError(source.to_string(), source)
+        }
+    }
+
+    // TODO: propagate exception from the server in a less brittle way
+    fn is_replica_not_primary(err: &GrpcMessageError) -> bool {
+        err.grpc_status == GrpcStatus::Internal as i32 && err.grpc_message.contains("[RPL01]")
+    }
+
+    fn is_token_credential_invalid(err: &GrpcMessageError) -> bool {
+        err.grpc_status == GrpcStatus::Unauthenticated as i32 && err.grpc_message.contains("[CLS08]")
     }
 }
