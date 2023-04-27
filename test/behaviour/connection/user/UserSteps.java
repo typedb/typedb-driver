9f8cecb703529190e05950c820bf046b050664e5
James Williams <james.williams@vaticle.com>
2023-04-27 09:33:38 +0100

    test: depend on fork of behaviour

--- test/behaviour/connection/user/UserSteps.java
+++ test/behaviour/connection/user/UserSteps.java
@@ -47,6 +47,11 @@ public class UserSteps {
         return users.contains(username);
     }
 
+    @Then("user get self")
+    public void users_get() {
+        User ignored = getClient().user();
+    }
+
     @Then("users get user: {word}")
     public void users_get(String username) {
         User ignored = getClient().users().get(username);
