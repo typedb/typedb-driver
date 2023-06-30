da7f0e9598bade04be956e17f9e74bcb38b4000d
Shiladitya Mukherjee <shiladitya@vaticle.com>
2023-06-30 11:38:33 +0100

    removed unused import

--- test/behaviour/util/UtilSteps.java
+++ test/behaviour/util/UtilSteps.java
@@ -24,7 +24,6 @@ package com.vaticle.typedb.client.test.behaviour.util;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-import java.util.Calendar;
 import java.util.TimeZone;
 
 public class UtilSteps {
