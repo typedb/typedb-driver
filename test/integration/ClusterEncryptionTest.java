ffc50e172f25d2a2fd1f13445dac6cb4b83b3c97
Krishnan Govindraj <krishnan@vaticle.com>
2023-07-24 15:57:50 +0100

    Cleanup test dependencies; small refactors

--- test/integration/ClusterEncryptionTest.java
+++ test/integration/ClusterEncryptionTest.java
@@ -46,10 +46,14 @@ import static org.junit.Assert.fail;
 public class ClusterEncryptionTest {
 
     private static final Logger LOG = LoggerFactory.getLogger(ClusterEncryptionTest.class);
-    private static final String ADMIN_USERNAME = "admin";
-    private static final String ADMIN_PASSWORD = "password";
+    private static final String USERNAME = "admin";
+    private static final String PASSWORD = "password";
     private static final Path CLUSTER_CA = Path.of("test/integration/resources/dev-root-ca.pem");
     private static final Path WRONG_CA = Path.of("test/integration/resources/wrong-root-ca.pem");
+
+    private static final TypeDBCredential ENCRYPTED_CREDENTIALS = new TypeDBCredential(USERNAME, PASSWORD, CLUSTER_CA);
+    private static final TypeDBCredential MISCONFIGURED_CREDENTIALS = new TypeDBCredential(USERNAME, PASSWORD, WRONG_CA);
+    private static final TypeDBCredential UNENCRYPTED_CREDENTIALS = new TypeDBCredential(USERNAME, PASSWORD, false);
 
     private TypeDBClusterRunner clusterRunner;
 
@@ -72,20 +76,20 @@ public class ClusterEncryptionTest {
         createCluster(Collections.singletonMap("--server.encryption.enable", "true"));
         clusterRunner.start();
         try {
-            try (TypeDBClient.Cluster client = TypeDB.clusterClient(clusterRunner.externalAddresses(), new TypeDBCredential(ADMIN_USERNAME, ADMIN_PASSWORD, CLUSTER_CA))) {
+            try (TypeDBClient.Cluster client = TypeDB.clusterClient(clusterRunner.externalAddresses(), ENCRYPTED_CREDENTIALS)) {
                 LOG.info("Encrypted client connected successfully");
             } catch (Exception e) {
                 fail("The encrypted client with correct CA failed to connect.");
             }
 
-            try (TypeDBClient.Cluster client = TypeDB.clusterClient(clusterRunner.externalAddresses(), new TypeDBCredential(ADMIN_USERNAME, ADMIN_PASSWORD, WRONG_CA))) {
+            try (TypeDBClient.Cluster client = TypeDB.clusterClient(clusterRunner.externalAddresses(), MISCONFIGURED_CREDENTIALS)) {
                 fail("The encrypted client with wrong CA connected (should have failed).");
             } catch (TypeDBClientException e) {
                 Assert.assertEquals(e.getErrorMessage(), CLUSTER_SSL_HANDSHAKE_FAILED);
                 LOG.info("Expected exception was thrown: {}", e.getMessage());
             }
 
-            try (TypeDBClient.Cluster client = TypeDB.clusterClient(clusterRunner.externalAddresses(), new TypeDBCredential(ADMIN_USERNAME, ADMIN_PASSWORD, false))) {
+            try (TypeDBClient.Cluster client = TypeDB.clusterClient(clusterRunner.externalAddresses(), UNENCRYPTED_CREDENTIALS)) {
                 fail("The encrypted client with wrong CA connected (should have failed).");
             } catch (TypeDBClientException e) {
                 // Best we can do given the grpc message
@@ -102,13 +106,13 @@ public class ClusterEncryptionTest {
         createCluster(Collections.singletonMap("--server.encryption.enable", "false"));
         clusterRunner.start();
         try {
-            try (TypeDBClient.Cluster client = TypeDB.clusterClient(clusterRunner.externalAddresses(), new TypeDBCredential(ADMIN_USERNAME, ADMIN_PASSWORD, false))) {
+            try (TypeDBClient.Cluster client = TypeDB.clusterClient(clusterRunner.externalAddresses(), UNENCRYPTED_CREDENTIALS)) {
                 LOG.info("Unencrypted client connected successfully!");
             } catch (Exception e) {
                 fail("The unencrypted client failed to connect to unencrypted server.");
             }
 
-            try (TypeDBClient.Cluster client = TypeDB.clusterClient(clusterRunner.externalAddresses(), new TypeDBCredential(ADMIN_USERNAME, ADMIN_PASSWORD, CLUSTER_CA))) {
+            try (TypeDBClient.Cluster client = TypeDB.clusterClient(clusterRunner.externalAddresses(), ENCRYPTED_CREDENTIALS)) {
                 fail("The encrypted client connected to an unencrypted server (should have failed).");
             } catch (TypeDBClientException e) {
                 Assert.assertEquals(e.getErrorMessage(), CLUSTER_SERVER_NOT_ENCRYPTED);
