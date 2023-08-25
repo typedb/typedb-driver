28da4542ccddfadaf56187b3ae62322716a97f52
Dmitrii Mikhalin <17214250+dmikhalin@users.noreply.github.com>
2023-08-25 17:34:16 +0100

    Try Windows

---
+++ .circleci/windows/deploy.bat
@@ -0,0 +1,26 @@
+REM
+REM Copyright (C) 2021 Vaticle
+REM
+REM This program is free software: you can redistribute it and/or modify
+REM it under the terms of the GNU Affero General Public License as
+REM published by the Free Software Foundation, either version 3 of the
+REM License, or (at your option) any later version.
+REM
+REM This program is distributed in the hope that it will be useful,
+REM but WITHOUT ANY WARRANTY; without even the implied warranty of
+REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+REM GNU Affero General Public License for more details.
+REM
+REM You should have received a copy of the GNU Affero General Public License
+REM along with this program.  If not, see <https://www.gnu.org/licenses/>.
+REM
+
+REM needs to be called such that software installed
+REM by Chocolatey in prepare.bat is accessible
+CALL refreshenv
+
+ECHO Building and deploying windows package...
+SET DEPLOY_PIP_USERNAME=%REPO_VATICLE_USERNAME%
+SET DEPLOY_PIP_PASSWORD=%REPO_VATICLE_PASSWORD%
+git rev-parse HEAD > bazel run --define version=%1 //python:deploy-pip -- snapshot
+IF %errorlevel% NEQ 0 EXIT /b %errorlevel%
