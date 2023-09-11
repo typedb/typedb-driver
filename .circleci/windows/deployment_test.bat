c594cd6f08ca57a7668c3a0f98f1617963c4300f
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2023-09-11 15:35:35 +0100

    revert to start /b for typedb server

--- .circleci/windows/deployment_test.bat
+++ .circleci/windows/deployment_test.bat
@@ -25,7 +25,7 @@ CALL refreshenv
 
 bazel --output_user_root=C:\bazel build @vaticle_typedb_artifact_windows//file
 7z x bazel-typedb-driver-java\external\vaticle_typedb_artifact_windows\file\typedb-server-windows-1409dac6c21a9d6eda5f35734ad51ddaab2a4e3c.zip
-typedb-server-windows-1409dac6c21a9d6eda5f35734ad51ddaab2a4e3c\typedb server
+START /B "" typedb-server-windows-1409dac6c21a9d6eda5f35734ad51ddaab2a4e3c\typedb server
 
 powershell -Command "(gc java\test\deployment\pom.xml) -replace 'CLIENT_JAVA_VERSION_MARKER', '%CIRCLE_SHA1%' | Out-File -encoding ASCII java\test\deployment\pom.xml"
 type java\test\deployment\pom.xml
