76e1ee2bbb260a970d5c8c4b5d3e9d5aef79ddca
Dmitrii Mikhalin <17214250+dmikhalin@users.noreply.github.com>
2023-09-20 14:57:50 +0100

    typedoc rules (need to be run)

--- nodejs/doc/TypeDocRunner.ts
+++ nodejs/doc/TypeDocRunner.ts
@@ -22,24 +22,17 @@
 const TypeDoc = require("typedoc");
 
 async function main() {
-    // Application.bootstrap also exists, which will not load plugins
-    // Also accepts an array of option readers if you want to disable
-    // TypeDoc's tsconfig.json/package.json/typedoc.json option readers
-    console.log("\nCurrent directory:", process.cwd());
+    console.log("Current directory:", process.cwd());
     const app = await TypeDoc.Application.bootstrapWithPlugins({
-        entryPoints: ["index.js", "api/**"],
-        entryPointStrategy: "resolve",
+        entryPoints: ["index.ts"],
     });
 
     const project = await app.convert();
 
     if (project) {
-        // Project may not have converted correctly
         const outputDir = "docs";
 
-        // Rendered docs
         await app.generateDocs(project, outputDir);
-        // Alternatively generate JSON output
         await app.generateJson(project, outputDir + "/documentation.json");
     }
 }
