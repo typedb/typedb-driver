0b78efd0bc235c457a34cb68e821c933375ffbdf
Dmitrii Mikhalin <17214250+dmikhalin@users.noreply.github.com>
2023-10-05 11:44:01 +0100

    CI target for docs check

---
+++ tool/docs/update.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+#
+# Copyright (C) 2022 Vaticle
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+bazel run //java:driver_adoc
+bazel run //nodejs:driver_adoc
+bazel run //python:driver_adoc
+bazel run //rust:driver_adoc_async
+bazel run //rust:driver_adoc_sync
