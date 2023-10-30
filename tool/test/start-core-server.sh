8819b9fe0b2ad9e15a9cf35328b90264fb1eb0dc
Krishnan Govindraj <krishnan@vaticle.com>
2023-10-30 10:39:57 +0000

    Revise how JAVA_HOME is set in start-core-server

--- tool/test/start-core-server.sh
+++ tool/test/start-core-server.sh
@@ -25,7 +25,8 @@ set -e
 rm -rf typedb-all
 
 bazel run //tool/test:typedb-extractor -- typedb-all
-JAVA_HOME=$(bazel run //tool/test:echo-java-home) ./typedb-all/typedb server &
+BAZEL_JAVA_HOME=$(bazel run //tool/test:echo-java-home)
+JAVA_HOME=$BAZEL_JAVA_HOME ./typedb-all/typedb server &
 
 set +e
 POLL_INTERVAL_SECS=0.5
