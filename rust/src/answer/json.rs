add46ad67c5c8a52d58673882d16cf3fc98b2b65
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2023-11-29 11:31:33 +0000

    JSON unit test

--- rust/src/answer/json.rs
+++ rust/src/answer/json.rs
@@ -111,3 +111,19 @@ fn write_escaped_string(string: &str, f: &mut fmt::Formatter<'_>) -> fmt::Result
 
     write!(f, r#""{}""#, unsafe { String::from_utf8_unchecked(buf) })
 }
+
+#[cfg(test)]
+mod test {
+    use std::borrow::Cow;
+
+    use super::JSON;
+
+    #[test]
+    fn test_against_serde() {
+        let string: String =
+            (0u8..0x7fu8).map(|byte| byte as char).chain("lorem ипсум どぉる　سيتامعت".chars()).collect();
+        let serde_json_value = serde_json::value::Value::String(string.clone());
+        let json_string = JSON::String(Cow::Owned(string));
+        assert_eq!(serde_json::to_string(&serde_json_value).unwrap(), json_string.to_string());
+    }
+}
