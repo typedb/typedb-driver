63834481659e7f63460462825f6043eec87d421b
Krishnan Govindraj <krishnan@vaticle.com>
2023-12-05 19:01:58 +0000

    Consume pkg_files for dylib

--- cpp/test/integration/test.cpp
+++ cpp/test/integration/test.cpp
@@ -28,103 +28,98 @@
 
 using namespace TypeDB;
 
+void delete_if_exists(const TypeDB::Driver& driver, const std::string& name) {
+    if (driver.databases.contains(name)) {
+        driver.databases.get(name).drop();
+    }
+}
 
-// void delete_if_exists(const TypeDB::Driver& driver, const std::string& name) {
-//     if (driver.databases.contains(name)) {
-//         driver.databases.get(name).drop();
-//     }
-// }
+TEST(TestConceptAPI, TestData) {
+    std::string dbName = "hello_from_cpp";
+    TypeDB::Driver driver = TypeDB::Driver::coreDriver("127.0.0.1:1729");
+    delete_if_exists(driver, dbName);
+    driver.databases.create(dbName);
 
-// TEST(TestConceptAPI, TestData) {
-//     std::string dbName = "hello_from_cpp";
-//     TypeDB::Driver driver = TypeDB::Driver::coreDriver("127.0.0.1:1729");
-//     delete_if_exists(driver, dbName);
-//     driver.databases.create(dbName);
-
-//     try {
-//         driver.databases.create(dbName);
-//         FAIL();  // "Exception not thrown"
-//     } catch (TypeDBDriverException e) {
-//         std::cout << "Caught exception with message: " << e.message() << std::endl;
-//     }
-// }
+    try {
+        driver.databases.create(dbName);
+        FAIL();  // "Exception not thrown"
+    } catch (TypeDBDriverException e) {
+        std::cout << "Caught exception with message: " << e.message() << std::endl;
+    }
+}
 
 
-// TEST(TestExplanations, TestExplainableOwnership) {
-//     std::string dbName = "test_explanations";
-//     TypeDB::Driver driver = TypeDB::Driver::coreDriver("127.0.0.1:1729");
-//     int attrValue = 42;
-//     std::string ruleLabel = "ownership-generation";
-//     std::string then = "$a has $a";
-//     std::string when = "$a isa attr;";
-//     std::regex whenRegex = std::regex("\\{\\s*\\$a isa attr;\\s*\\}");
+TEST(TestExplanations, TestExplainableOwnership) {
+    std::string dbName = "test_explanations";
+    TypeDB::Driver driver = TypeDB::Driver::coreDriver("127.0.0.1:1729");
+    int attrValue = 42;
+    std::string ruleLabel = "ownership-generation";
+    std::string then = "$a has $a";
+    std::string when = "$a isa attr;";
+    std::regex whenRegex = std::regex("\\{\\s*\\$a isa attr;\\s*\\}");
 
-//     delete_if_exists(driver, dbName);
-    
-//     driver.databases.create(dbName);
-//     TypeDB::Options options;
+    delete_if_exists(driver, dbName);
 
-//     {
-//         auto sess = driver.session(dbName, TypeDB::SessionType::SCHEMA, options);
-//         auto tx = sess.transaction(TypeDB::TransactionType::WRITE, options);
-//         std::string schema =
-//             " define attr sub attribute, value long, owns attr;"
-//             "rule " +
-//             ruleLabel + ": when { " + when + " } then { " + then + "; }; ";
-//         tx.query.define(schema, options).wait();
+    driver.databases.create(dbName);
+    TypeDB::Options options;
 
-//         tx.commit();
-//     }
+    {
+        auto sess = driver.session(dbName, TypeDB::SessionType::SCHEMA, options);
+        auto tx = sess.transaction(TypeDB::TransactionType::WRITE, options);
+        std::string schema =
+            " define attr sub attribute, value long, owns attr;"
+            "rule " +
+            ruleLabel + ": when { " + when + " } then { " + then + "; }; ";
+        tx.query.define(schema, options).wait();
 
-//     {
-//         auto sess = driver.session(dbName, TypeDB::SessionType::DATA, options);
-//         auto tx = sess.transaction(TypeDB::TransactionType::WRITE, options);
+        tx.commit();
+    }
 
-//         auto res = tx.query.insert("insert $o " + std::to_string(attrValue) + " isa attr;", options);
-//         for (auto& it : res)
-//             ;
-//         tx.commit();
-//     }
+    {
+        auto sess = driver.session(dbName, TypeDB::SessionType::DATA, options);
+        auto tx = sess.transaction(TypeDB::TransactionType::WRITE, options);
 
-//     {
-//         TypeDB::Options explainOptions;
-//         explainOptions.infer(true).explain(true);
+        auto res = tx.query.insert("insert $o " + std::to_string(attrValue) + " isa attr;", options);
+        for (auto& it : res)
+            ;
+        tx.commit();
+    }
 
-//         auto sess = driver.session(dbName, TypeDB::SessionType::DATA, options);
-//         auto tx = sess.transaction(TypeDB::TransactionType::READ, explainOptions);
+    {
+        TypeDB::Options explainOptions;
+        explainOptions.infer(true).explain(true);
 
-//         auto res = tx.query.get("match $o1 has $o2; get;", options);
-//         for (TypeDB::ConceptMap& cm : res) {
-//             for (std::string& v : cm.variables()) {
-//                 ASSERT_TRUE(v == "o1" || v == "o2");
-//                 ASSERT_EQ(attrValue, cm.get(v)->asAttribute()->getValue()->asLong());
-//             }
-//             for (auto& ownsKey : cm.explainables().ownerships()) {
-//                 auto explainable = cm.explainables().ownership(ownsKey.owner, ownsKey.attribute);
-//                 for (auto& explanation : tx.query.explain(explainable, options)) {
-//                     ASSERT_EQ(ruleLabel, explanation.rule().label());
-//                     ASSERT_EQ(then, explanation.rule().then());
-//                     ASSERT_TRUE(std::regex_match(explanation.rule().when(), whenRegex));
-//                     // std::cout << explanation.rule().then() << " :- " << explanation.rule().when() <<  std::endl;
+        auto sess = driver.session(dbName, TypeDB::SessionType::DATA, options);
+        auto tx = sess.transaction(TypeDB::TransactionType::READ, explainOptions);
 
-//                     auto conclusion = explanation.conclusion();
-//                     for (std::string& v : conclusion.variables()) {
-//                         ASSERT_EQ(v, "a");
-//                         ASSERT_EQ(attrValue, conclusion.get(v)->asAttribute()->getValue()->asLong());
-//                     }
-//                     auto condition = explanation.condition();
-//                     for (std::string& v : condition.variables()) {
-//                         ASSERT_EQ(v, "a");
-//                         ASSERT_EQ(attrValue, conclusion.get(v)->asAttribute()->getValue()->asLong());
-//                     }
-//                 }
-//             }
-//         }
-//     }
-// }
+        auto res = tx.query.get("match $o1 has $o2; get;", options);
+        for (TypeDB::ConceptMap& cm : res) {
+            for (std::string& v : cm.variables()) {
+                ASSERT_TRUE(v == "o1" || v == "o2");
+                ASSERT_EQ(attrValue, cm.get(v)->asAttribute()->getValue()->asLong());
+            }
+            for (auto& ownsKey : cm.explainables().ownerships()) {
+                auto explainable = cm.explainables().ownership(ownsKey.owner, ownsKey.attribute);
+                for (auto& explanation : tx.query.explain(explainable, options)) {
+                    ASSERT_EQ(ruleLabel, explanation.rule().label());
+                    ASSERT_EQ(then, explanation.rule().then());
+                    ASSERT_TRUE(std::regex_match(explanation.rule().when(), whenRegex));
+                    // std::cout << explanation.rule().then() << " :- " << explanation.rule().when() <<  std::endl;
 
-TEST(TestIterInvalid,TestIterInvalid) {
-    std::cout << TypeDB::InternalError::ILLEGAL_STATE.formatString << std::endl;
+                    auto conclusion = explanation.conclusion();
+                    for (std::string& v : conclusion.variables()) {
+                        ASSERT_EQ(v, "a");
+                        ASSERT_EQ(attrValue, conclusion.get(v)->asAttribute()->getValue()->asLong());
+                    }
+                    auto condition = explanation.condition();
+                    for (std::string& v : condition.variables()) {
+                        ASSERT_EQ(v, "a");
+                        ASSERT_EQ(attrValue, conclusion.get(v)->asAttribute()->getValue()->asLong());
+                    }
+                }
+            }
+        }
+    }
 }
 
 int main(int argc, char** argv) {
