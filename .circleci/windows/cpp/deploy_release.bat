71488cde8a6d8604e2ccc1ca5e228042f09b5c32
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2024-01-25 13:55:07 +0000

    Only deploy releases to cloudsmith (#578)
    ## Usage and product changes
    
    We implement the following changes to the release process:
    
    - stop uploading build artifacts to the github releases page;
    - update the release notes documentation links;
    - add C and C++ artifact download link templates to the release notes
    template.

--- .circleci/windows/cpp/deploy_release.bat
+++ .circleci/windows/cpp/deploy_release.bat
@@ -30,6 +30,3 @@ SET DEPLOY_ARTIFACT_PASSWORD=%REPO_TYPEDB_PASSWORD%
 SET /p VER=<VERSION
 bazel --output_user_root=C:\bazel run --verbose_failures --define version=%VER% //cpp:deploy-windows-x86_64-zip --compilation_mode=opt -- release
 IF %errorlevel% NEQ 0 EXIT /b %errorlevel%
-
-MD dist
-COPY bazel-bin\cpp\typedb-driver-cpp-windows-x86_64.zip dist\typedb-driver-cpp-windows-x86_64.zip
