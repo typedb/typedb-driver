32b9109d6b1af155d32fa7cf5d07ccdbc9f7ef23
Novoselov <georgii@vaticle.com>
2024-01-29 18:05:41 +0000

    Start C# implementation, add base connectivity interfaces and implementations

---
+++ csharp/api/TypeDBCredential.cs
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2022 Vaticle
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+using System.Diagnostics;
+
+using com.vaticle.typedb.driver.pinvoke;
+using com.vaticle.typedb.driver.Common;
+
+namespace com.vaticle.typedb.driver.Api
+{
+    /**
+     * User credentials and TLS encryption settings for connecting to TypeDB Cloud.
+     *
+     * <h3>Examples</h3>
+     * <pre>
+     * // Creates a credential as above, but the connection will be made over TLS.
+     * TypeDBCredential credential = new TypeDBCredential(username, password, true);
+     *
+     * // Creates a credential as above, but TLS will use the specified CA to authenticate server certificates.
+     * TypeDBCredential credential = new TypeDBCredential(username, password, Path.of("path/to/ca-certificate.pem"));
+     * </pre>
+     */
+    public class TypeDBCredential: NativeObject<pinvoke.Credential>
+    {
+        /**
+         *
+         * @param username The name of the user to connect as
+         * @param password The password for the user
+         * @param tlsEnabled Specify whether the connection to TypeDB Cloud must be done over TLS
+         */
+        public TypeDBCredential(string username, string password, bool tlsEnabled)
+            : this(username, password, null, tlsEnabled)
+        {}
+
+        /**
+         *
+         * @param username The name of the user to connect as
+         * @param password The password for the user
+         * @param tlsRootCAPath Path to the CA certificate to use for authenticating server certificates.
+         */
+        public TypeDBCredential(string username, string password, Path tlsRootCAPath) // TODO: Path
+            : this(username, password, tlsRootCAPath.ToString(), true)
+        {}
+
+        private TypeDBCredential(string username, string password, string? tlsRootCAPath, bool tlsEnabled)
+            : base(NativeCredential(username, password, tlsRootCAPath, tlsEnabled))
+        {}
+
+        private static pinvoke.Credential NativeCredential(
+            string username, string password, string? tlsRootCAPath, bool tlsEnabled)
+        {
+            Debug.Assert(tlsEnabled || tlsRootCAPath == null);
+
+            try
+            {
+                return pinvoke.credential_new(username, password, tlsRootCAPath, tlsEnabled);
+            }
+            catch (pinvoke.Error error)
+            {
+                throw new TypeDBDriverException(error);
+            }
+        }
+    }
+}
---
+++ csharp/common/Label.cs
@@ -0,0 +1,165 @@
+/*
+ * Copyright (C) 2022 Vaticle
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+namespace com.vaticle.typedb.driver.Common
+{
+    /**
+     * A <code>Label</code> holds the uniquely identifying name of a type.
+     * <p>It consists of an optional <code>scope</code>, and a <code>name</code>, represented <code>scope:name</code>.
+     * The scope is used only used to distinguish between role-types of the same name declared in different relation types.
+     */
+    public class Label
+    {
+        private readonly string _scope;
+        private readonly string _name;
+        private readonly int _hash;
+
+        private Label(string? scope, string name)
+        {
+            this.scope = scope;
+            this.name = name;
+            this.hash = Objects.hash(name, scope);
+        }
+
+        /**
+         * Creates a Label from a specified name.
+         *
+         * <h3>Examples</h3>
+         * <pre>
+         * Label.Of("entity");
+         * </pre>
+         *
+         * @param name Label name
+         */
+        public static Label Of(string name)
+        {
+            return new Label(null, name);
+        }
+
+        /**
+         * Creates a Label from a specified scope and name.
+         *
+         * <h3>Examples</h3>
+         * <pre>
+         * Label.Of("relation", "role");
+         * </pre>
+         *
+         * @param scope Label scope
+         * @param name Label name
+         */
+        public static Label of(string scope, string name)
+        {
+            return new Label(scope, name);
+        }
+
+        /**
+         * Returns the scope of this Label.
+         *
+         * <h3>Examples</h3>
+         * <pre>
+         * label.Scope();
+         * </pre>
+         */
+        public string? Scope()
+        {
+            return _scope;
+        }
+
+        /**
+         * Returns the name of this Label.
+         *
+         * <h3>Examples</h3>
+         * <pre>
+         * label.Name();
+         * </pre>
+         */
+        public string Name()
+        {
+            return _name;
+        }
+
+        /**
+         * Returns the string representation of the scoped name.
+         *
+         * <h3>Examples</h3>
+         * <pre>
+         * label.ScopedName();
+         * </pre>
+         */
+        public string ScopedName()
+        {
+            if (scope == null)
+            {
+                return _name;
+            }
+
+            return _scope + ":" + _name;
+        }
+
+        /**
+         * Returns the string representation of the scoped name.
+         *
+         * <h3>Examples</h3>
+         * <pre>
+         * label.ToString();
+         * </pre>
+         */
+        public override string ToString()
+        {
+            return ScopedName();
+        }
+
+        /**
+         * Checks if this Label is equal to another object.
+         *
+         * <h3>Examples</h3>
+         * <pre>
+         * label.Equals(obj);
+         * </pre>
+         *
+         * @param obj Object to compare with
+         */ // TODO: Implement for C#
+        public override bool Equals(Object obj)
+        {
+            if (this == obj)
+            {
+                return true;
+            }
+
+            if (obj == null || getClass() != obj.getClass())
+            {
+                return false;
+            }
+
+            Label that = (Label)obj;
+
+            return this.name.equals(that.name) && Objects.equals(this.scope, that.scope);
+        }
+
+        /**
+         * @hidden
+         */
+        public override int HashCode()
+        {
+            return hash;
+        }
+    }
+}
