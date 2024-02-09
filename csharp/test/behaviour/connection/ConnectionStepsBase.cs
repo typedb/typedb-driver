a550484f4a864a1085d72f6f2328c858b5f93295
Georgii Novoselov <georgii@vaticle.com>
2024-02-09 11:54:34 +0000

    Add additional structures to bdd tests. Fix cloud bdd tests for session

--- csharp/test/behaviour/connection/ConnectionStepsBase.cs
+++ csharp/test/behaviour/connection/ConnectionStepsBase.cs
@@ -47,8 +47,23 @@ namespace Vaticle.Typedb.Driver.Test.Behaviour
             }
 
             Sessions.Clear();
+            SessionsToTransactions.Clear();
+
             Task.WaitAll(ParallelSessions.ToArray());
             ParallelSessions.Clear();
+
+            foreach (var (session, transactions) in SessionsToParallelTransactions)
+            {
+                Task.WaitAll(transactions.ToArray());
+            }
+            SessionsToParallelTransactions.Clear();
+
+            foreach (var (session, transactions) in ParallelSessionsToParallelTransactions)
+            {
+                session.Wait();
+                Task.WaitAll(transactions.ToArray());
+            }
+            ParallelSessionsToParallelTransactions.Clear();
 
             if (Driver != null)
             {
@@ -62,6 +77,11 @@ namespace Vaticle.Typedb.Driver.Test.Behaviour
                     Driver.Close();
                 }
             }
+        }
+
+        public ITypeDBTransaction? SingleTransaction
+        {
+            get { return SessionsToTransactions[Sessions[0]][0]; }
         }
 
         public abstract ITypeDBDriver CreateTypeDBDriver(string address);
@@ -100,13 +120,29 @@ namespace Vaticle.Typedb.Driver.Test.Behaviour
         }
 
         public static ITypeDBDriver Driver;
+
         public static List<ITypeDBSession?> Sessions = new List<ITypeDBSession?>();
         public static List<Task<ITypeDBSession?>> ParallelSessions = new List<Task<ITypeDBSession?>>();
-        public static TypeDBOptions SessionOptions;
-        public static TypeDBOptions TransactionOptions;
+
         public static Dictionary<ITypeDBSession, List<ITypeDBTransaction?>> SessionsToTransactions =
             new Dictionary<ITypeDBSession, List<ITypeDBTransaction?>>();
-        public static Dictionary<Task<ITypeDBSession?>, List<ITypeDBTransaction?>> ParallelSessionsToTransactions =
-            new Dictionary<Task<ITypeDBSession?>, List<ITypeDBTransaction?>>();
+        public static Dictionary<ITypeDBSession?, List<Task<ITypeDBTransaction?>>> SessionsToParallelTransactions =
+            new Dictionary<ITypeDBSession?, List<Task<ITypeDBTransaction?>>>();
+        public static Dictionary<Task<ITypeDBSession?>, List<Task<ITypeDBTransaction?>>> ParallelSessionsToParallelTransactions =
+            new Dictionary<Task<ITypeDBSession?>, List<Task<ITypeDBTransaction?>>>();
+
+        public static TypeDBOptions SessionOptions;
+        public static TypeDBOptions TransactionOptions;
+
+        public static readonly Dictionary<string, Action<TypeDBOptions, string>> OptionSetters =
+            new Dictionary<string, Action<TypeDBOptions, string>>(){
+                {"session-idle-timeout-millis", (option, val) => option.SessionIdleTimeoutMillis(Int32.Parse(val))},
+                {"transaction-timeout-millis", (option, val) => option.TransactionTimeoutMillis(Int32.Parse(val))}
+        };
+
+        public static readonly Dictionary<string, string> ServerOptions =
+            new Dictionary<string, string>(){
+                {"--diagnostics.reporting.enable", "false"}
+        };
     }
 }
