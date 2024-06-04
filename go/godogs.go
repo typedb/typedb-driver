4f374bace08490b4a513b7ba559c820866987a1e
nickan <nickan2c@gmail.com>
2024-06-04 19:42:47 +0100

    Working version. Now trying to get the dependencies go.bzl to correctly give us what we need

--- go/godogs.go
+++ go/godogs.go
@@ -1,6 +1,24 @@
 package main
 
-// Godogs available to eat
-var Godogs int
+// this path is interesting - can use gopath or something
 
-func main() { /* usual main func */ }
+/*
+#cgo LDFLAGS: -Lswigdir -ltypedb_driver_go_native -framework CoreFoundation
+*/
+import "C"
+import (
+	"fmt"
+	//"go/swigdir" we actually need to import a go_lib which contains the path to the go wrapper
+)
+
+func openCoreFunc() {
+	fmt.Println("hi")
+
+	value := 667
+	driver_value := typedb_driver.Value_new_long(int64(value))
+	fmt.Println(typedb_driver.Value_get_long(driver_value))
+}
+
+func main() {
+	openCoreFunc()
+}
