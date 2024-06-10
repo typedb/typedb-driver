175b09a0ba661369c3b0e8c2531d10fc57cd0f9d
nickan <nickan2c@gmail.com>
2024-06-10 14:09:55 +0100

    working with symbol not found error

--- go/godogs.go
+++ go/godogs.go
@@ -3,20 +3,29 @@ package main
 // this path is interesting - can use gopath or something
 
 /*
-#cgo LDFLAGS: -Lswigdir -ltypedb_driver_go_native -framework CoreFoundation
+#cgo LDFLAGS: -Lgo/ -ltypedb_driver_go_native -framework CoreFoundation
 */
 import "C"
 import (
 	"fmt"
-	//"go/swigdir" we actually need to import a go_lib which contains the path to the go wrapper
+	"os"
+	"example/go_wrapper"
 )
 
 func openCoreFunc() {
 	fmt.Println("hi")
+	pwd, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(pwd)
+
 
 	value := 667
 	driver_value := typedb_driver.Value_new_long(int64(value))
 	fmt.Println(typedb_driver.Value_get_long(driver_value))
+
 }
 
 func main() {
