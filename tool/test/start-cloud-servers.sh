ce932ec63317876d9e90ab079fc4fd7af4194455
Georgii Novoselov <georgii@vaticle.com>
2024-06-13 17:49:18 +0100

    Fix cloud runners init

--- tool/test/start-cloud-servers.sh
+++ tool/test/start-cloud-servers.sh
@@ -45,6 +45,7 @@ function server_start() {
     --server.encryption.file.internal-grpc.root-ca=`realpath tool/test/resources/encryption/int-grpc-root-ca.pem` \
     --server.encryption.file.internal-zmq.private-key=`realpath tool/test/resources/encryption/int-zmq-private-key` \
     --server.encryption.file.internal-zmq.public-key=`realpath tool/test/resources/encryption/int-zmq-public-key` \
+    --diagnostics.monitoring.port=${1}1732 \
     --development-mode.enable=true
 }
 
