410e6c6e401a6a630eb627e50f1aea3b00fe3e92
Georgii Novoselov <22564079+farost@users.noreply.github.com>
2024-06-14 10:07:09 +0100

    Remove excessive diagnostics flags, turn on development mode for tests (#659)
    ## Usage and product changes
    We clean the usage of `--diagnostics.***` flags used in the test servers
    because of the recent propagation of a newer version of `TypeDB`, now
    supporting the `--development-mode.enable` flag. As the development mode
    includes disabling any outer reporting, it is a perfect substitution for
    this logic.
    
    ## Implementation
    We remove the unnecessary flags everywhere, aiming to cover it by the
    activated development mode.
    
    * This mode is activated by default for all the `TypeDB***Runner`s
    [relevant for the `Java` driver]
    * This mode is **not** activated for `vaticle_typedb_artifact` fetched
    by `vaticle_typedb_artifact()` or built by `bazel build
    @vaticle_typedb_artifact_*platform*//file`, so we need to manually add
    `--development-mode.enable=true` for all the invocations of these
    artifacts.

--- tool/test/start-cloud-servers.sh
+++ tool/test/start-cloud-servers.sh
@@ -45,9 +45,8 @@ function server_start() {
     --server.encryption.file.internal-grpc.root-ca=`realpath tool/test/resources/encryption/int-grpc-root-ca.pem` \
     --server.encryption.file.internal-zmq.private-key=`realpath tool/test/resources/encryption/int-zmq-private-key` \
     --server.encryption.file.internal-zmq.public-key=`realpath tool/test/resources/encryption/int-zmq-public-key` \
-    --diagnostics.reporting.errors=false \
-    --diagnostics.reporting.statistics=false \
-    --diagnostics.monitoring.enable=false
+    --diagnostics.monitoring.port=${1}1732 \
+    --development-mode.enable=true
 }
 
 rm -rf $(seq 1 $NODE_COUNT) typedb-cloud-all
