134f7968ec33294a6f61f53afc0df7b865eae272
Ganeshwara Hananda <ganesh@vaticle.com>
2024-11-20 13:44:06 +0000

    Authentication - wip

--- rust/tests/integration/core/tmp.rs
+++ rust/tests/integration/core/tmp.rs
@@ -6,11 +6,11 @@ use typedb_driver::{Credential, TypeDBDriver};
 fn tmp() {
     async_std::task::block_on(async {
         let driver: TypeDBDriver = TypeDBDriver::new_core(TypeDBDriver::DEFAULT_ADDRESS, Credential::without_tls("admin", "password")).await.unwrap();
-        let users = driver.users();
+        // let users = driver.users();
         // let x = users.contains("test").await;
         // users.create("user", "password").await.unwrap();
         // users.get("user").await.unwrap()
-        users.delete("user").await.unwrap()
+        // users.delete("user").await.unwrap()
     });
     todo!()
 }
