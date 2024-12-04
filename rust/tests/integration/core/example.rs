d627bcaf4602bab54973d06ea6fa0ec9b606c89e
Ganeshwara Hananda <ganesh@vaticle.com>
2024-12-04 10:05:33 +0000

    Fix a few missed instances that needed to be renamed

--- rust/tests/integration/core/example.rs
+++ rust/tests/integration/core/example.rs
@@ -28,7 +28,7 @@ use typedb_driver::{
         ConceptRow, QueryAnswer,
     },
     concept::{Concept, ValueType},
-    Credential, DriverOptions, Error, TransactionType, TypeDBDriver,
+    Credentials, DriverOptions, Error, TransactionType, TypeDBDriver,
 };
 
 // EXAMPLE END MARKER
@@ -36,7 +36,7 @@ use typedb_driver::{
 async fn cleanup() {
     let driver = TypeDBDriver::new_core(
         TypeDBDriver::DEFAULT_ADDRESS,
-        Credential::new("admin", "password"),
+        Credentials::new("admin", "password"),
         DriverOptions::new(false, None).unwrap(),
     )
     .await
@@ -57,7 +57,7 @@ fn example() {
         // Open a driver connection
         let driver = TypeDBDriver::new_core(
             TypeDBDriver::DEFAULT_ADDRESS,
-            Credential::new("admin", "password"),
+            Credentials::new("admin", "password"),
             DriverOptions::new(false, None).unwrap(),
         )
         .await
