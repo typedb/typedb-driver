03b3c30ae1b46c4190a64ee42d9cd7249b7a3021
joshua <joshua@typedb.com>
2024-12-19 12:37:25 -0500

    Clean up formatting

--- rust/src/connection/network/transmitter/transaction.rs
+++ rust/src/connection/network/transmitter/transaction.rs
@@ -290,7 +290,8 @@ impl TransactionTransmitter {
             }
         }
     }
-async fn listen_loop( mut grpc_source: Streaming<transaction::Server>,
+    async fn listen_loop(
+        mut grpc_source: Streaming<transaction::Server>,
         collector: ResponseCollector,
         shutdown_sink: UnboundedSender<()>,
     ) {
