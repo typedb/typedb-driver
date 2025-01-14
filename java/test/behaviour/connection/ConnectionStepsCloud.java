59298c543550a094f372b51c0366d21425cead35
Georgii Novoselov <georgii@typedb.com>
2025-01-14 15:29:09 +0000

    Actually create a cloud driver in java cloud bdds

--- java/test/behaviour/connection/ConnectionStepsCloud.java
+++ java/test/behaviour/connection/ConnectionStepsCloud.java
@@ -46,7 +46,7 @@ public class ConnectionStepsCloud extends ConnectionStepsBase {
 
     @Override
     Driver createTypeDBDriver(String address, Credentials credentials, DriverOptions driverOptions) {
-        return TypeDB.coreDriver(address, credentials, driverOptions);
+        return TypeDB.cloudDriver(address, credentials, driverOptions);
     }
 
     @Override
