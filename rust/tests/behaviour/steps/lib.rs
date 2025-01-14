9fbd2863ccfd3022420aed510c4d3509fdcf107c
Georgii Novoselov <georgii@typedb.com>
2025-01-14 14:59:31 +0000

    Fix java tests build and rust addresses

--- rust/tests/behaviour/steps/lib.rs
+++ rust/tests/behaviour/steps/lib.rs
@@ -130,7 +130,8 @@ impl fmt::Debug for Context {
 
 impl Context {
     const DEFAULT_CORE_ADDRESS: &'static str = "127.0.0.1:1729";
-    const DEFAULT_CLOUD_ADDRESSES: [&'static str; 3] = ["127.0.0.1:11729", "127.0.0.1:21729", "127.0.0.1:31729"];
+    // TODO when multiple nodes are available: "127.0.0.1:11729", "127.0.0.1:21729", "127.0.0.1:31729"
+    const DEFAULT_CLOUD_ADDRESSES: [&'static str; 1] = ["127.0.0.1:1729"];
     const DEFAULT_DATABASE: &'static str = "test";
     const ADMIN_USERNAME: &'static str = "admin";
     const ADMIN_PASSWORD: &'static str = "password";
