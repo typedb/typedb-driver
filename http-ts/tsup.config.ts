fe2c9f90580c7ab5215943605bf746ed0e6e2619
Alex Walker <alex@typedb.com>
2025-07-25 13:41:45 +0100

    Make the HTTP TypeScript driver dual-module (CJS+ESM)

---
+++ http-ts/tsup.config.ts
@@ -0,0 +1,25 @@
+import type { Options } from "tsup";
+
+const env = process.env.NODE_ENV;
+
+const tsup: Options = {
+    splitting: true,
+    sourcemap: true,
+    clean: true,
+    dts: true,
+    format: ["cjs", "esm"],
+    outExtension({ format }) {
+        return {
+            js: format === "esm" ? ".mjs" : ".cjs",
+        };
+    },
+    minify: false,
+    bundle: true,
+    skipNodeModulesBundle: true,
+    watch: env === "development",
+    target: "es2020",
+    outDir: "dist",
+    entry: ["src/index.ts"],
+};
+
+export default tsup;
