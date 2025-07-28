7f43decb4b9a3090b8411dba6cff20b80b93c16a
Alex Walker <alex@typedb.com>
2025-07-28 13:18:20 +0100

    Actually install nvm in CI lol

--- tool/http-ts/install-deps.sh
+++ tool/http-ts/install-deps.sh
@@ -16,8 +16,8 @@
 # specific language governing permissions and limitations
 # under the License.
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+\. "$HOME/.nvm/nvm.sh"
 nvm install 22
 nvm use 22
 npm install --global corepack@0.17.0
