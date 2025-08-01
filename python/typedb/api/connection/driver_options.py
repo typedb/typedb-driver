e38d6639833b00be63c299cba0b193db3dd687fa
Joshua Send <joshua@typedb.com>
2025-08-01 05:53:25 -0400

    Enable TLS by default in Python (#777)
    ## Usage and product changes
    
    We want to enable a secure-by-default setting in TypeDB Drivers. In Java
    and Rust, `DriverOptions` have to be explicitly set, and there are no
    defaults. However, Python features a disabled TLS default. While this is
    compatible with TypeDB CE, it's an insecure default & not compatible
    with TypeDB Cloud without explicitly enabling it.
    
    Instead, we set the default to TLS being __enabled__ in Python. This
    means when using an insecure, plaintext connection the user must
    explicitly set it, and is more likely to become aware of the plaintext
    communication.
    
    ## Implementation
    
    - Fix a documentation string in Rust
    - Set the default TLS connection in Python to be TLS enabled, instead of
    disabled
    
    - Update some forgotten HTTP-TS docs generation and testing targets

--- python/typedb/api/connection/driver_options.py
+++ python/typedb/api/connection/driver_options.py
@@ -25,17 +25,17 @@ from typedb.native_driver_wrapper import driver_options_new, DriverOptions as Na
 class DriverOptions(NativeWrapper[NativeDriverOptions]):
     """
     User credentials and TLS encryption settings for connecting to TypeDB Server. Arguments:
-    1) is_tls_enabled: Specify whether the connection to TypeDB Cloud must be done over TLS.
+    1) is_tls_enabled: Specify whether the connection to TypeDB must be done over TLS.
     2) tls_root_ca_path: Path to the CA certificate to use for authenticating server certificates.
 
     Examples:
     --------
     ::
 
-        driver_options = DriverOptions(tls_enabled=True, tls_root_ca_path="path/to/ca-certificate.pem")
+        driver_options = DriverOptions(is_tls_enabled=True, tls_root_ca_path="path/to/ca-certificate.pem")
     """
 
-    def __init__(self, is_tls_enabled: bool = False, tls_root_ca_path: Optional[str] = None):
+    def __init__(self, is_tls_enabled: bool = True, tls_root_ca_path: Optional[str] = None):
         super().__init__(driver_options_new(is_tls_enabled, tls_root_ca_path))
 
     @property
