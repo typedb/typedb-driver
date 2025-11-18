f080b9adf8bd313628c3d0d65400e0afe4cd0270
Krishnan Govindraj <krishnan@typedb.com>
2025-11-18 14:53:42 +0000

    Remove legacy type aliases that have no structural difference

--- http-ts/src/legacy.ts
+++ http-ts/src/legacy.ts
@@ -20,19 +20,9 @@
 // Backwards compatibility for server versions < 3.7.0
 import {
     ConstraintVertexLabel,
-    ConstraintVertexValue,
     ConstraintVertexVariable,
-    ConstraintComparison,
-    ConstraintFunction,
-    ConstraintHas, ConstraintIid, ConstraintIs,
-    ConstraintIsa,
-    ConstraintIsaExact, ConstraintKind, ConstraintLabel,
-    ConstraintOwns,
-    ConstraintPlays,
-    ConstraintRelates, ConstraintSpan,
-    ConstraintSub,
-    ConstraintSubExact, ConstraintValue,
-    VariableInfo,
+    ConstraintSpan,
+    VariableInfo, ConstraintAny,
 } from "./analyzed-conjunction";
 import {ConceptRowAnswer, QueryType} from "./response";
 
@@ -47,7 +37,7 @@ export interface ConceptRowsQueryResponseLegacy {
 }
 
 export interface QueryConjunctionLegacy {
-    constraints: QueryConstraintAny[]
+    constraints: ConstraintAny[] | ConstraintExpressionLegacy | ConstraintLinksLegacy
 }
 
 export interface QueryStructureLegacy {
@@ -73,50 +63,3 @@ export interface ConstraintExpressionLegacy {
     arguments: ConstraintVertexVariable[],
     assigned: ConstraintVertexVariable[],
 }
-
-// Legacy type names:
-// We don't export it because we want everyone to upgrade.
-/*export*/ type QueryStructure = QueryStructureLegacy;
-/*export*/ type QueryVertex = QueryVertexVariable | QueryVertexLabel | QueryVertexValue;
-/*export*/ type QueryVertexVariable = ConstraintVertexVariable;
-/*export*/ type QueryVertexValue = ConstraintVertexValue;
-/*export*/ type QueryVertexLabel = ConstraintVertexLabel;
-/*export*/ type QueryVariableInfo = VariableInfo;
-
-/*export*/ type QueryConstraintSpan = ConstraintSpan;
-/*export*/ type QueryConstraintIsa = ConstraintIsa;
-/*export*/ type QueryConstraintIsaExact = ConstraintIsaExact;
-/*export*/ type QueryConstraintHas = ConstraintHas;
-/*export*/ type QueryConstraintLinks = ConstraintLinksLegacy;
-/*export*/ type QueryConstraintSub = ConstraintSub;
-/*export*/ type QueryConstraintSubExact = ConstraintSubExact;
-/*export*/ type QueryConstraintOwns = ConstraintOwns;
-/*export*/ type QueryConstraintRelates = ConstraintRelates;
-/*export*/ type QueryConstraintPlays = ConstraintPlays;
-/*export*/ type QueryConstraintExpression = ConstraintExpressionLegacy;
-/*export*/ type QueryConstraintFunction = ConstraintFunction;
-/*export*/ type QueryConstraintComparison = ConstraintComparison;
-/*export*/ type QueryConstraintIs = ConstraintIs;
-/*export*/ type QueryConstraintIid = ConstraintIid;
-/*export*/ type QueryConstraintKind = ConstraintKind;
-/*export*/ type QueryConstraintValue = ConstraintValue;
-/*export*/ type QueryConstraintLabel = ConstraintLabel;
-
-/*export*/ type QueryConstraintAny =
-    QueryConstraintIsa
-    | QueryConstraintIsaExact
-    | QueryConstraintHas
-    | QueryConstraintLinks
-    | QueryConstraintSub
-    | QueryConstraintSubExact
-    | QueryConstraintOwns
-    | QueryConstraintRelates
-    | QueryConstraintPlays
-    | QueryConstraintExpression
-    | QueryConstraintFunction
-    | QueryConstraintComparison
-    | QueryConstraintIs
-    | QueryConstraintIid
-    | QueryConstraintKind
-    | QueryConstraintValue
-    | QueryConstraintLabel;
