fd31c398768e9a26369601ddd3a4374d4709c128
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2026-04-15 17:52:44 +0100

    Revert "make it pub so it's not culled?"
    This reverts commit f59976770127a7164bb06050b9ef40fb1aad9393.

--- rust/tests/behaviour/steps/connection/mod.rs
+++ rust/tests/behaviour/steps/connection/mod.rs
@@ -27,7 +27,7 @@ mod user;
 
 #[apply(generic_step)]
 #[step("typedb starts")]
-pub async fn typedb_starts(_: &mut Context) {}
+async fn typedb_starts(_: &mut Context) {}
 
 #[apply(generic_step)]
 #[step("connection opens with default authentication")]
