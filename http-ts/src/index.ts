fd796ca2ec8dd3258846be9c553a21e9cbb1051c
Krishnan Govindraj <krishnan@typedb.com>
2026-06-17 14:15:58 +0200

    Update http-ts to include literalrs

--- http-ts/src/index.ts
+++ http-ts/src/index.ts
@@ -390,5 +390,9 @@ export interface User {
     username: string;
 }
 
-export type GivenRowEntry = Value | Entity | Relation | Attribute;
+export type GivenRowEntry = Value | Entity | Relation | Attribute | LiteralRowEntry;
+export type LiteralRowEntry =
+    boolean // for boolean values
+    | number  // For integer and double values
+    | string; // For all other types, as well as IIDs.
 export type GivenRows = { [varName: string]: GivenRowEntry }[];
